a98ef818fcbd3f1f93754196fbadbef9a4e01c57
officialluq <lukaszprzybylik@gmail.com>
2024-02-28 20:45:12 +0100

    fix: fixed include file for orangepi5

--- include/configs/rk3588_common.h
+++ include/configs/rk3588_common.h
@@ -26,7 +26,8 @@
 	"ramdisk_addr_r=0x12180000\0"	\
 	"ramdisk_addr_r_temp=-\0"	\
 	"kernel_comp_size=0x8000000\0"  \
-	"mmc_dev=1\0"
+	"mmc_dev=1\0" \
+	"mmcpart=3\0" 
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
@@ -34,16 +35,17 @@
 	ENV_MEM_LAYOUT_SETTINGS			\
 	ROCKCHIP_DEVICE_SETTINGS \
 	"kernel_name=/boot/Image\0"		\
-	"mmcargs=setenv bootargs root=${rootdev} rw rootwait;\0"		\
+	"mmcargs=setenv bootargs root=${rootdev} init=/sbin/init rw rootwait;\0"		\
 	BOOT_SETTINGS \
 	"boot_targets=" BOOT_TARGETS "\0"
 
 #define BOOT_SETTINGS \ 
 	"load_fdt= " \
-		"load mmc ${mmc_dev}:${mmcpart} ${fdt_addr_r} ${fdtfile} ${fdt_load_args};\0" \
+		"load mmc ${mmc_dev}:${mmcpart} ${fdt_addr_r} /boot/${fdtfile} ${fdt_load_args};\0" \
 	"load_kernel= " \
 		"load mmc ${mmc_dev}:${mmcpart} ${kernel_addr_r} ${kernel_name} ${kernel_load_args};\0" \
 	"custom_boot= " \
+		"mmc dev ${mmc_dev}; " \
 		"run load_fdt; " \
 		"run load_kernel; " \
 		"run mmcargs; " \
